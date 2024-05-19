e43a3924a3f02168ce8e1003ac36d3b0b6149edc
Abraham Livinus <abrahamlivinus@gmail.com>
2024-05-19 07:12:40 -0700

    add

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-/states_list: HTML page with a list of all State objects in DBStorage.
-"""
+'''crates a flask web application'''
 from flask import Flask, render_template
 from models import storage
-from models.state import State
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.teardown_appcontext
-def close_db(exc):
-    """close the current session of sqlalchemist"""
-    storage.close()
-
+app.url_map.strict_slashes=False
 
 @app.route('/states_list')
-def states_list():
-    """
-    Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+def display_states():
+    return render_template('7-states_list.html', storage=storage.all('State'))
 
+@app.teardown_appcontext
+def close(exception):
+    storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
